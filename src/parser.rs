814a9601db43a638e46242f24f68b5ae49fc8a51
hi-kbys <hiroyuki.kobayashi@estie.co.jp>
2024-09-29 18:36:51 +0900

    3.9 四則演算の構文木

--- src/parser.rs
+++ src/parser.rs
@@ -1,179 +1,129 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha1, alphanumeric1, char, multispace0},
+    combinator::recognize,
+    error::ParseError,
+    multi::{fold_many0, many0},
+    number::complete::recognize_float,
+    sequence::{delimited, pair},
+    IResult, Parser,
+};
+
 fn main() {
-    let input = "2 + 3 + 4";
-    println!("expr: {:?}", expr(input));
-    let input = "2+3+4 + ( 5 + 6 )";
-    println!("expr: {:?}", expr(input));
+    fn ex_eval<'src>(input: &'src str) -> Result<f64, nom::Err<nom::error::Error<&'src str>>> {
+        expr(input).map(|(_, e)| eval(e))
+    }
+    let input = "123";
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
+
+    let input = "(123 + 456 ) + pi";
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
+
+    let input = "10 + (100 - 1)";
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
+
+    let input = "((1 + 2) + (3 + 4)) + 5 + 6";
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
+
+    let input = "1 + 2 * 3";
+    println!("source: {:?}, parsed: {:?}", input, ex_eval(input));
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Expression<'src> {
-    Indent(&'src str),
+    Ident(&'src str),
     NumLiteral(f64),
     Add(Box<Expression<'src>>, Box<Expression<'src>>),
+    Sub(Box<Expression<'src>>, Box<Expression<'src>>),
+    Mul(Box<Expression<'src>>, Box<Expression<'src>>),
+    Div(Box<Expression<'src>>, Box<Expression<'src>>),
 }
 
-fn expr(input: &str) -> Option<(&str, Expression)> {
-    if let Some(res) = add(input) {
-        return Some(res);
-    }
-    if let Some(res) = term(input) {
-        return Some(res);
-    }
-    None
-}
-
-fn add(mut input: &str) -> Option<(&str, Expression)> {
-    let mut left = None;
-    while let Some((next_input, expr)) = add_term(input) {
-        if let Some(prev_left) = left {
-            left = Some(Expression::Add(Box::new(prev_left), Box::new(expr)))
-        } else {
-            left = Some(expr)
-        }
-        input = next_input;
-    }
-    let left = left?;
-    let (next_input, rhs) = expr(input)?;
-    Some((next_input, Expression::Add(Box::new(left), Box::new(rhs))))
-}
-
-fn add_term(input: &str) -> Option<(&str, Expression)> {
-    let (next_input, lhs) = term(input)?;
-    let (next_input, _) = plus(whitespace(next_input))?;
-    Some((next_input, lhs))
-}
-
-fn term(input: &str) -> Option<(&str, Expression)> {
-    if let Some(res) = paren(input) {
-        return Some(res);
-    }
-    if let Some(res) = token(input) {
-        return Some(res);
-    }
-    None
-}
-
-fn paren(input: &str) -> Option<(&str, Expression)> {
-    let next_input = lparen(whitespace(input))?;
-    let (next_input, expr) = expr(next_input)?;
-    let next_input = rparen(whitespace(next_input))?;
-    Some((next_input, expr))
-}
-
-fn token(i: &str) -> Option<(&str, Expression)> {
-    if let Some(res) = ident(whitespace(i)) {
-        return Some(res);
-    }
-    if let Some(res) = number(whitespace(i)) {
-        return Some(res);
-    }
-    None
-}
-
-fn whitespace(mut input: &str) -> &str {
-    while matches!(peek_char(input), Some(' ')) {
-        input = advance_char(input);
-    }
-    input
-}
-
-fn plus(mut input: &str) -> Option<(&str, Expression)> {
-    if matches!(peek_char(input), Some('+')) {
-        input = advance_char(input);
-        Some((input, Expression::Indent("+")))
-    } else {
-        None
+fn eval(expr: Expression) -> f64 {
+    match expr {
+        Expression::Ident("pi") => std::f64::consts::PI,
+        Expression::Ident(id) => panic!("Unknown name {:?}", id),
+        Expression::NumLiteral(n) => n,
+        Expression::Add(lhs, rhs) => eval(*lhs) + eval(*rhs),
+        Expression::Sub(lhs, rhs) => eval(*lhs) - eval(*rhs),
+        Expression::Mul(lhs, rhs) => eval(*lhs) * eval(*rhs),
+        Expression::Div(lhs, rhs) => eval(*lhs) / eval(*rhs),
     }
 }
 
-fn number(mut input: &str) -> Option<(&str, Expression)> {
-    let start = input;
-    if matches!(peek_char(input), Some(_x @ ('-' | '+' | '0'..='9'))) {
-        input = advance_char(input);
-        while matches!(peek_char(input), Some(_x @ ('.' | '0'..='9'))) {
-            input = advance_char(input);
-        }
-        if let Ok(num) = start[..(start.len() - input.len())].parse::<f64>() {
-            Some((input, Expression::NumLiteral(num)))
-        } else {
-            None
-        }
-    } else {
-        None
-    }
+fn expr(input: &str) -> IResult<&str, Expression> {
+    let (i, init) = term(input)?;
+
+    fold_many0(
+        pair(space_delimited(alt((char('+'), char('-')))), term),
+        move || init.clone(),
+        |acc, (_op, val): (char, Expression)| match _op {
+            '+' => Expression::Add(Box::new(acc), Box::new(val)),
+            '-' => Expression::Sub(Box::new(acc), Box::new(val)),
+            _ => panic!(
+                "sub or add expression should be here, '+' or '-' but got {:?}",
+                _op
+            ),
+        },
+    )(i)
 }
 
-fn ident(mut input: &str) -> Option<(&str, Expression)> {
-    let start = input;
-    if matches!(peek_char(input), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
-        while matches!(
-            peek_char(input),
-            Some(_x @ ('a'..='z' | 'A'..='Z' | '0'..='9'))
-        ) {
-            input = advance_char(input);
-        }
-        Some((
-            input,
-            Expression::Indent(&start[..(start.len() - input.len())]),
-        ))
-    } else {
-        None
-    }
+fn term(input: &str) -> IResult<&str, Expression> {
+    let (i, init) = factor(input)?;
+
+    fold_many0(
+        pair(space_delimited(alt((char('*'), char('/')))), factor),
+        move || init.clone(),
+        |acc, (_op, val): (char, Expression)| match _op {
+            '*' => Expression::Mul(Box::new(acc), Box::new(val)),
+            '/' => Expression::Div(Box::new(acc), Box::new(val)),
+            _ => panic!(
+                "Multiplicative Expression should be here, '*' or '/' but got {:?}",
+                _op
+            ),
+        },
+    )(i)
 }
 
-fn lparen(mut input: &str) -> Option<&str> {
-    if matches!(peek_char(input), Some('(')) {
-        input = advance_char(input);
-        Some(input)
-    } else {
-        None
-    }
+fn factor(input: &str) -> IResult<&str, Expression> {
+    alt((number, ident, parens))(input)
 }
 
-fn rparen(mut input: &str) -> Option<&str> {
-    if matches!(peek_char(input), Some(')')) {
-        input = advance_char(input);
-        Some(input)
-    } else {
-        None
-    }
+fn parens(input: &str) -> IResult<&str, Expression> {
+    delimited(tag("("), expr, tag(")"))(input)
 }
 
-fn advance_char(input: &str) -> &str {
-    let mut chars = input.chars();
-    chars.next();
-    chars.as_str()
+fn number(input: &str) -> IResult<&str, Expression> {
+    let (r, v) = space_delimited(recognize_float)(input)?;
+    Ok((
+        r,
+        Expression::NumLiteral(v.parse().map_err(|_| {
+            nom::Err::Error(nom::error::Error {
+                input,
+                code: nom::error::ErrorKind::Digit,
+            })
+        })?),
+    ))
 }
 
-fn peek_char(input: &str) -> Option<char> {
-    input.chars().next()
+fn ident(input: &str) -> IResult<&str, Expression> {
+    let (r, v) = space_delimited(identifier)(input)?;
+    Ok((r, Expression::Ident(v)))
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+fn identifier(input: &str) -> IResult<&str, &str> {
+    recognize(pair(
+        alt((alpha1, tag("_"))),
+        many0(alt((alphanumeric1, tag("_")))),
+    ))(input)
+}
 
-//     #[test]
-//     fn test_whitespace() {
-//         assert_eq!(whitespace("  "), "");
-//         assert_eq!(whitespace("  a"), "a");
-//         assert_eq!(whitespace("a"), "a");
-//     }
-
-//     #[test]
-//     fn test_number() {
-//         assert_eq!(number("123"), "");
-//         assert_eq!(number("123.456"), "");
-//         assert_eq!(number("123.456a"), "a");
-//         assert_eq!(number("a"), "a");
-//         assert_eq!(number("-123"), "");
-//     }
-
-//     #[test]
-//     fn test_ident() {
-//         assert_eq!(ident("abc"), "");
-//         assert_eq!(ident("abc123"), "");
-//         assert_eq!(ident("abc123a"), "");
-//         assert_eq!(ident("123"), "123");
-//     }
-// }
+fn space_delimited<'src, O, E>(
+    f: impl Parser<&'src str, O, E>,
+) -> impl FnMut(&'src str) -> IResult<&'src str, O, E>
+where
+    E: ParseError<&'src str>,
+{
+    delimited(multispace0, f, multispace0)
+}
